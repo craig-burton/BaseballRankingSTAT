4d5b57828537cfb46ee61677d986666aa57e5934
Reese Murphy <reesemurphy@sbcglobal.net>
2017-10-25 17:19:38 -0500

    graph change

--- pitcher_ERA.py
+++ pitcher_ERA.py
@@ -4,6 +4,7 @@ from matplotlib import style
 import mysql.connector
 import numpy as np
 import networkx
+import csv
 
 
 config = pd.read_csv('config.csv')
@@ -24,74 +25,66 @@ team_ids = pd.read_sql(query,con=cnx)
 team_id_dict = {}
 for index,row in team_ids.iterrows():
     team_id_dict[row['home_team_id']] = index
-    print(row['home_team_id'] + ": " + str(index))
+#     print(row['home_team_id'] + ": " + str(index))
+#
+# incidence = [[0 for x in range(len(team_id_dict))] for y in range(len(team_id_dict))]
+#
+# def update_incidence(array_of_rows):
+#     #if home team won, add 1 to win_loss, if away_team_id won substract 1
+#     if(len(array_of_rows) == 0):
+#         return;
+#     # print(len(array_of_rows))
+#     win_loss = 0
+#     home_team_id = team_id_dict[array_of_rows[0]['HOME_TEAM_ID']]
+#     away_team_id = team_id_dict[array_of_rows[0]['AWAY_TEAM_ID']]
+#     for row in array_of_rows:
+#         if(row['HOME_SCORE_CT'] > row['AWAY_SCORE_CT']):
+#             win_loss += 1
+#         elif(row['HOME_SCORE_CT'] < row['AWAY_SCORE_CT']):
+#             win_loss -= 1
+#     if(win_loss >= 1):
+#         #home team won
+#         incidence[home_team_id][away_team_id] += 1
+#     elif(win_loss < 0):
+#         incidence[away_team_id][home_team_id] += 1
+#     else:
+#         incidence[home_team_id][away_team_id] += .5
+#         incidence[away_team_id][home_team_id] += .5
+#
+# #second step: loop through team's home series
+# for team,num in team_id_dict.items():
+#     #check the teams home series
+#     query = "select * from games where home_team_id = '" \
+#         + team + "' and game_yr = 2016 order by game_dt;"
+#     home_series = pd.read_sql(query,con=cnx)
+#     last_team_played = home_series.loc[0]['AWAY_TEAM_ID']
+#     last_game_dt = 0
+#     last_games = []
+#     team_count = 0
+#     for index,row in home_series.iterrows():
+#         #two cases: played the last team you played, or not
+#         if(row['AWAY_TEAM_ID'] == last_team_played):
+#             #keep adding to the array of last_games
+#             last_games.append(row)
+#             last_game_dt = row['GAME_DT']
+#         else:
+#             #stop adding to the array of last_games
+#             update_incidence(last_games)
+#             team_count += 1
+#             last_game_dt = row['GAME_DT']
+#             last_team_played = row['AWAY_TEAM_ID']
+#             last_games = [row]
+#
+# with open("incidence.csv", "w") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(team_id_dict.keys())
+#     writer.writerows(incidence)
 
-incidence = [[0 for x in range(len(team_id_dict))] for y in range(len(team_id_dict))]
+# print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+#       for row in incidence]))
+# print(len(incidence))
 
-def update_incidence(array_of_rows):
-
-    #if home team won, add 1 to win_loss, if away_team_id won substract 1
-    if(len(array_of_rows) == 0):
-        return;
-    # print(len(array_of_rows))
-    win_loss = 0
-    home_team_id = team_id_dict[array_of_rows[0]['HOME_TEAM_ID']]
-    away_team_id = team_id_dict[array_of_rows[0]['AWAY_TEAM_ID']]
-    for row in array_of_rows:
-        # print(str(row['HOME_SCORE_CT']) + " " + str(row['AWAY_SCORE_CT']))
-        if(row['HOME_SCORE_CT'] > row['AWAY_SCORE_CT']):
-            win_loss += 1
-        elif(row['HOME_SCORE_CT'] < row['AWAY_SCORE_CT']):
-            win_loss -= 1
-    if(win_loss >= 1):
-        #home team won
-        incidence[home_team_id][away_team_id] += 1
-        # print("home team: " + str(home_team_id) + " won against " + str(away_team_id))
-    elif(win_loss < 0):
-        incidence[away_team_id][home_team_id] += 1
-        # print("away team: " + str(away_team_id) + " won against " + str(home_team_id))
-    else:
-        incidence[home_team_id][away_team_id] += .5
-        incidence[away_team_id][home_team_id] += .5
-
-        # print("ITS A TIE home: " + str(home_team_id) + " away: " + str(away_team_id))
-
-#second step: loop through team's home series
-for team,num in team_id_dict.items():
-    #check the teams home series
-    query = "select * from games where home_team_id = '" \
-        + team + "' and game_yr = 2016 order by game_dt;"
-    home_series = pd.read_sql(query,con=cnx)
-    # print("TEAM = " + team + " ID = " + str(team_id_dict[team]))
-    last_team_played = home_series.loc[0]['AWAY_TEAM_ID']
-    last_game_dt = 0
-    last_games = []
-    team_count = 0
-    for index,row in home_series.iterrows():
-        #two cases: played the last team you played, or not
-
-        if(row['AWAY_TEAM_ID'] == last_team_played):
-            #keep adding to the array of last_games
-            last_games.append(row)
-            last_game_dt = row['GAME_DT']
-        else:
-            #stop adding to the array of last_games
-            update_incidence(last_games)
-            team_count += 1
-            last_game_dt = row['GAME_DT']
-            last_team_played = row['AWAY_TEAM_ID']
-            last_games = [row]
-    # print(team_count)
-
-
-# for r in incidence:
-#     print(r)
-
-print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in incidence]))
-print(len(incidence))
-
-
+#
 #TODO: Pythagorean Win/Loss
 team_pythag = {}
 team_totals = []
@@ -136,10 +129,10 @@ def actual_win_loss(team_id,curr_game_dt,look_back=50):
 # print(pythagorean_win_loss("TEX",20161201,look_back=162,power=2))
 # print(actual_win_loss("TEX",20161201,look_ back=162))
 
-#Pick 2016 and plot all of teams pythagorean_win_loss
+#Pick 2015 and plot all of teams pythagorean_win_loss
 win_loss = [0 for x in range(len(team_id_dict))]
 pythag = [0 for x in range(len(team_id_dict))]
-line = [.5 for x in range(len(team_id_dict))]
+line = [.35,.65]
 diff = [0 for x in range(len(team_id_dict))]
 for team, num in team_id_dict.items():
     win_loss[num] = actual_win_loss(team,20151201,look_back=162)
@@ -147,13 +140,21 @@ for team, num in team_id_dict.items():
     diff[num] = win_loss[num] - pythag[num]
 style.use('ggplot')
 
+# x = sorted(team_id_dict.values())
+# plt.scatter(x,win_loss,label="Actual",color='b')
+# xticks = sorted(team_id_dict,key=team_id_dict.get)
+# plt.scatter(x,pythag,label="Pythagorean",color='r')
+# plt.plot(x,line, color='k', linestyle='--', label='.500')
+# plt.xticks(x,xticks)
+# plt.legend()
+# plt.title('Actual vs. Expected Win-Loss 2015')
+
 x = sorted(team_id_dict.values())
-plt.scatter(x,win_loss,label="Actual",color='b')
+plt.scatter(win_loss,pythag,color='b')
 xticks = sorted(team_id_dict,key=team_id_dict.get)
-plt.scatter(x,pythag,label="Pythagorean",color='r')
-plt.plot(x,line, color='k', linestyle='--', label='.500')
-plt.xticks(x,xticks)
-plt.legend()
+plt.xlabel('Actual Win-Loss')
+plt.ylabel('Pythagorean Win-Loss')
+plt.plot(line,line, color='k', linestyle='--', label='Act = Pythag')
 plt.title('Actual vs. Expected Win-Loss 2015')
 
 zero = [0 for i in range(len(team_id_dict))]
