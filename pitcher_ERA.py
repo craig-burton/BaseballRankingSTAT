9d6017705c94d455fcb6969e73693a4bb8421b4a
craig-burton <cburton@trinity.edu>
2017-11-01 14:12:30 -0500

    Added page rank, error between actual win/loss and expected win/loss

--- pitcher_ERA.py
+++ pitcher_ERA.py
@@ -21,16 +21,15 @@ cnx = mysql.connector.connect(user=config.loc[0]['USER'],password=config.loc[0][
 #TODO: incidence matrices
 
 #first step: find all the teams, assign them id in a dictionary
-query = "select distinct home_team_id from games where game_yr = 2016 order by home_team_id;"
+query = "select distinct home_team_id from games where game_yr = 2015 order by home_team_id;"
 team_ids = pd.read_sql(query,con=cnx)
 team_id_dict = {}
 for index,row in team_ids.iterrows():
     team_id_dict[row['home_team_id']] = index
     print(row['home_team_id'] + ": " + str(index))
-
 incidence = [[0 for x in range(len(team_id_dict))] for y in range(len(team_id_dict))]
 
-def update_incidence(array_of_rows):
+def update_incidence(array_of_rows,incidence):
     #if home team won, add 1 to win_loss, if away_team_id won substract 1
     if(len(array_of_rows) == 0):
         return;
@@ -51,12 +50,13 @@ def update_incidence(array_of_rows):
     else:
         incidence[home_team_id][away_team_id] += .5
         incidence[away_team_id][home_team_id] += .5
+    return incidence
 
 #second step: loop through team's home series
 for team,num in team_id_dict.items():
     #check the teams home series
     query = "select * from games where home_team_id = '" \
-        + team + "' and game_yr = 2016 order by game_dt;"
+        + team + "' and game_yr = 2015 order by game_dt;"
     home_series = pd.read_sql(query,con=cnx)
     last_team_played = home_series.loc[0]['AWAY_TEAM_ID']
     last_game_dt = 0
@@ -70,7 +70,7 @@ for team,num in team_id_dict.items():
             last_game_dt = row['GAME_DT']
         else:
             #stop adding to the array of last_games
-            update_incidence(last_games)
+            incidence = update_incidence(last_games,incidence)
             team_count += 1
             last_game_dt = row['GAME_DT']
             last_team_played = row['AWAY_TEAM_ID']
@@ -78,6 +78,35 @@ for team,num in team_id_dict.items():
 
 incidence_np = np.matrix(incidence)
 print(incidence_np)
+
+def create_incidence(start_date,end_date):
+    incidence = [[0 for x in range(len(team_id_dict))] for y in range(len(team_id_dict))]
+    for team,num in team_id_dict.items():
+        #check the teams home series
+        query = "select * from games where home_team_id = '" \
+            + team + "' and game_dt >= " + str(start_date) + \
+            " and game_dt <= " + str(end_date) +  " order by game_dt;"
+        home_series = pd.read_sql(query,con=cnx)
+        last_team_played = home_series.loc[0]['AWAY_TEAM_ID']
+        last_game_dt = 0
+        last_games = []
+        team_count = 0
+        for index,row in home_series.iterrows():
+            #two cases: played the last team you played, or not
+            if(row['AWAY_TEAM_ID'] == last_team_played):
+                #keep adding to the array of last_games
+                last_games.append(row)
+                last_game_dt = row['GAME_DT']
+            else:
+                #stop adding to the array of last_games
+                incidence = update_incidence(last_games,incidence)
+                team_count += 1
+                last_game_dt = row['GAME_DT']
+                last_team_played = row['AWAY_TEAM_ID']
+                last_games = [row]
+
+    incidence_np = np.matrix(incidence)
+    return incidence_np
 # with open("incidence.csv", "w", newline='') as f:
 #     writer = csv.writer(f)
 #     writer.writerow([' '] + team_id_dict.keys())
@@ -132,19 +161,47 @@ def actual_win_loss(team_id,curr_game_dt,look_back=50):
                     losses += 1
         return wins/float(wins+losses)
 
+def pythagorean_win_loss_df(team_id,games,power=2):
+    runs_for = 0
+    runs_against = 0
+    for index,row in games.iterrows():
+        if(row['HOME_TEAM_ID'] == team_id):
+            runs_for += row['HOME_SCORE_CT']
+            runs_against += row['AWAY_SCORE_CT']
+        else:
+            runs_for += row['AWAY_SCORE_CT']
+            runs_against += row['HOME_SCORE_CT']
+    return (runs_for**power)/float(runs_for**power + runs_against**power)
+
+def actual_win_loss_df(team_id,games):
+    wins = 0
+    losses = 0
+    for index,row in games.iterrows():
+        if(row['HOME_TEAM_ID'] == team_id):
+            if(row['HOME_SCORE_CT'] > row['AWAY_SCORE_CT']):
+                wins += 1
+            else:
+                losses += 1
+        else:
+            if(row['AWAY_SCORE_CT'] > row['HOME_SCORE_CT']):
+                wins += 1
+            else:
+                losses += 1
+    return wins/float(wins+losses)
+
 # print(pythagorean_win_loss("TEX",20161201,look_back=162,power=2))
 # print(actual_win_loss("TEX",20161201,look_ back=162))
 
 #Pick 2015 and plot all of teams pythagorean_win_loss
-win_loss = [0 for x in range(len(team_id_dict))]
-pythag = [0 for x in range(len(team_id_dict))]
-line = [.35,.65]
-diff = [0 for x in range(len(team_id_dict))]
-for team, num in team_id_dict.items():
-    win_loss[num] = actual_win_loss(team,20151201,look_back=162)
-    pythag[num] = pythagorean_win_loss(team,20151201,look_back=162,power=2)
-    diff[num] = win_loss[num] - pythag[num]
-style.use('ggplot')
+# win_loss = [0 for x in range(len(team_id_dict))]
+# pythag = [0 for x in range(len(team_id_dict))]
+# line = [.35,.65]
+# diff = [0 for x in range(len(team_id_dict))]
+# for team, num in team_id_dict.items():
+#     win_loss[num] = actual_win_loss(team,20151201,look_back=162)
+#     pythag[num] = pythagorean_win_loss(team,20151201,look_back=162,power=2)
+#     diff[num] = win_loss[num] - pythag[num]
+# style.use('ggplot')
 
 # x = sorted(team_id_dict.values())
 # plt.scatter(x,win_loss,label="Actual",color='b')
@@ -155,25 +212,103 @@ style.use('ggplot')
 # plt.legend()
 # plt.title('Actual vs. Expected Win-Loss 2015')
 
-x = sorted(team_id_dict.values())
-plt.scatter(win_loss,pythag,color='b')
-xticks = sorted(team_id_dict,key=team_id_dict.get)
-plt.xlabel('Actual Win-Loss')
-plt.ylabel('Pythagorean Win-Loss')
-plt.plot(line,line, color='k', linestyle='--', label='Act = Pythag')
-plt.title('Actual vs. Expected Win-Loss 2015')
+# x = sorted(team_id_dict.values())
+# plt.scatter(win_loss,pythag,color='b')
+# xticks = sorted(team_id_dict,key=team_id_dict.get)
+# plt.xlabel('Actual Win-Loss')
+# plt.ylabel('Pythagorean Win-Loss')
+# plt.plot(line,line, color='k', linestyle='--', label='Act = Pythag')
+# plt.title('Actual vs. Expected Win-Loss 2015')
 
-zero = [0 for i in range(len(team_id_dict))]
-plt.figure()
-print(len(x),len(diff))
-plt.scatter(x,diff,label='Difference',color='g')
-plt.xticks(x,xticks)
-plt.plot(x,zero,color='k',linestyle='--',label='Zero')
-plt.legend()
-plt.title('Difference Between Actual and Expected Win-Loss 2015')
-plt.show()
+# zero = [0 for i in range(len(team_id_dict))]
+# plt.figure()
+# print(len(x),len(diff))
+# plt.scatter(x,diff,label='Difference',color='g')
+# plt.xticks(x,xticks)
+# plt.plot(x,zero,color='k',linestyle='--',label='Zero')
+# plt.legend()
+# plt.title('Difference Between Actual and Expected Win-Loss 2015')
+# # plt.show()
+
+# Compute errors of Pythag to win/loss and minimize using different alphas
+# alphas = np.arange(1,3,.1)
+# alphas_df = pd.DataFrame(alphas)
+# alphas_df["error"] = np.nan
+# print(alphas_df)
+# df_team_dict = {}
+# curr_game_dt = 20151201
+# look_back = 162
+# for team,num in team_id_dict.items():
+#     query = "select * from games where (home_team_id = '" + team \
+#             + "' or away_team_id = '" + team + "') and game_dt < " + str(curr_game_dt) \
+#             + " order by game_dt desc limit " + str(look_back) + ";"
+#     df_team_dict[team] = pd.read_sql(query,con=cnx)
+#
+# for i in alphas:
+#     sum_of_error = 0
+#     print("Alpha=" + str(i))
+#     for team, num in team_id_dict.items():
+#         sum_of_error += (actual_win_loss_df(team,df_team_dict[team]) - pythagorean_win_loss_df(team,df_team_dict[team],i))**2
+#     alphas_df.loc[alphas_df[0] == i,"error"] = sum_of_error
+# print(alphas_df)
+#
+# plt.plot(alphas_df[0],alphas_df["error"])
+#
+#
+# print(alphas_df.loc[alphas_df.idxmin()]) #1.71
+# plt.show()
+
+
+
+#Page Rank
+def page_rank(incidence,team_id_dict):
+    incidence = incidence_np.tolist()
+    for j in range(len(incidence[0])):
+        total = 0
+        for i in range(len(incidence)):
+            total += incidence[i][j]
+        if(total == 0):
+            for i in range(len(incidence)):
+                incidence[i][j] = 1/float(len(incidence))
+        else:
+            for i in range(len(incidence)):
+                incidence[i][j] = incidence[i][j] / float(total)
+
+    stochastic = np.matrix(incidence)
+    new = np.matrix(incidence)
+    for i in range(5000):
+        new = new * stochastic
+
+    ranking = {}
+    final = new.A
+    for x in team_id_dict:
+        ranking[x] =  final[team_id_dict[x]][0]
+
+    return ranking
+
+
+#Test page ranking model
+incidence = create_incidence(20150101,20150714)
+print(incidence)
+ranking = page_rank(incidence,team_id_dict)
+ranking_sorted = sorted(ranking,key=ranking.get,reverse=True)
+for x in ranking_sorted:
+    print x + " " + str(ranking[x])
+
+#Next make predictions based on these rankings
+#predict a game, save the result, update the incidence matrix using the actual result
+
+
+
+
+
 
 
 #TODO: Team Hit to Run
+
+
+
+
+
 
 #EOF
